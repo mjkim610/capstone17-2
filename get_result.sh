f4fb9c3f3068a86d0b1d8832b79609ccf58ac404
mjkim610 <mjkim610@gmail.com>
2017-08-27 14:42:59 +0900

    Update get_result.sh and remove unnecessary files

--- get_result.sh
+++ get_result.sh
@@ -1,8 +1,12 @@
 # Set variables
 VM_NAME="ubuntu16"
-ARG1='The quick brown fox jumps over the lazy dog.'
+ARG1="The quick brown fox jumps over the lazy dog."
 
+# Start vm and run code (SSH requires password prompt, so this isn't fully automated. It should be resolved, probably by using keys instead of passwords.)
 vboxmanage startvm $VM_NAME --type headless
 sleep 5
-ssh -p 2222 capstone@localhost "cd ~/capstone17-2/CalculateSHA1/src; java com.capstone.Main $ARG1;"
+ssh -p 2222 capstone@localhost "cd ~/capstone17-2/CalculateSHA1/src; java com.capstone.Main \"$ARG1\";"
 ssh -p 2222 capstone@localhost "cat ~/capstone17-2/CalculateSHA1/src/CalculateSHA1.txt" > result.txt
+
+# Turn off vm
+vboxmanage controlvm $VM_NAME acpipowerbutton
