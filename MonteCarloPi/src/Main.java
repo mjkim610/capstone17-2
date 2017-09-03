d0a6269d9c647a8073f788506a5e065fc42b2194
mjkim610 <mjkim610@gmail.com>
2017-09-03 17:12:04 +0900

    Refactor MonteCarloPi

--- MonteCarloPi/src/Main.java
+++ MonteCarloPi/src/Main.java
@@ -2,7 +2,7 @@ import java.lang.Math;
 
 public class Main {
 
-    public static double throwDart(int r, int n) {
+    public static double estimatePi(int r, int n) {
         double x;
         double y;
 
@@ -16,13 +16,15 @@ public class Main {
                 bullseyeCount++;
             }
         }
-        return (double)bullseyeCount/(double)n;
+        return 4.0*(double)bullseyeCount/(double)n;
     }
 
     public static void main(String[] args) {
-        System.out.print("Program argument (r): ");
+        String output = "";
+
+        output = output + "Program argument (r): ";
         for (int i=0; i<args.length; i++) {
-            System.out.println(args[i]);
+            output = output + args[i] + "\n";
         }
 
         int r = Integer.parseInt(args[0]);
@@ -30,12 +32,12 @@ public class Main {
 
         long startTime = System.nanoTime();
 
-        double ratio = throwDart(r, n);
-        double piEstimate = ratio*4.0;
-        System.out.println("Pi Estimate: " + piEstimate);
+        double piEstimate = estimatePi(r, n);
+        output = output + "Pi Estimate: " + piEstimate + "\n";
 
         long duration = System.nanoTime() - startTime;
-        System.out.println("Duration: " + duration);
+        output = output + "Duration: " + duration + "\n";
 
+        System.out.println(output);
     }
 }
